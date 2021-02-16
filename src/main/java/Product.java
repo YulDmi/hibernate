30095c6d232e786f5884f962c3ddd71a30855ca5
YulDmi <barsetca@rambler.ru>
2021-02-16 19:58:08 +0300

    lesson_5

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,3 +1,47 @@
-public class Product {
+import javax.persistence.*;
+import java.util.List;
 
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    public Product(String title) {
+        this.title = title;
+    }
+
+    public Product(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title ='" + title + '\'' +
+                '}';
+    }
 }
